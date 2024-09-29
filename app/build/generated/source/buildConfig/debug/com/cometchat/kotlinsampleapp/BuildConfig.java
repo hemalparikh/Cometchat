81928ea44c4409f2f64ddaf2b29fedbe206a656d
hemalparikh <168854901+hemalparikh@users.noreply.github.com>
2024-09-29 22:27:26 +0530

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/cometchat/kotlinsampleapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cometchat.kotlinsampleapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.cometchat.kotlinsampleapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "4.3.13";
+}
